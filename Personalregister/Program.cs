e341bd61047a840af06f721f03d8cfe03c747d71
Sarbast Hesso <sarbast.hesso@gmail.com>
2025-04-01 16:51:06 +0200

    Add project files.

---
+++ Personalregister/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Personalregister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pleas enter the first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Pleas enter the last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Pleas enter the salary: ");
+            int salary = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\n {firstName} {lastName} / {salary}");
+        }
+    }
+}
