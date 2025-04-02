12b458678d197986407d2bbd4c57273244032b3c
Sarbast Hesso <sarbast.hesso@gmail.com>
2025-04-02 12:47:25 +0200

    updated

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -2,15 +2,60 @@
 {
     internal class Program
     {
+
+        static List<(string name, int salary)> employee = new List<(string, int)>();
+
         static void Main(string[] args)
         {
-            Console.Write("Pleas enter the first name: ");
-            string firstName = Console.ReadLine();
-            Console.Write("Pleas enter the last name: ");
-            string lastName = Console.ReadLine();
-            Console.Write("Pleas enter the salary: ");
-            int salary = int.Parse(Console.ReadLine());
-            Console.WriteLine($"\n {firstName} {lastName} / {salary}");
+            while (true)
+            {
+                Console.WriteLine("Choose 1 for add an employee");
+                Console.WriteLine("Choose 2 to show list of employee");
+                Console.WriteLine("Choose 3 to close");
+                Console.WriteLine("Please, enter your choice");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    addEmployee();
+                }
+                else if (choice == "2")
+                {
+                    showList();
+                }
+                else if (choice == "3")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice");
+                }
+            }
+        }
+
+        static void addEmployee()
+        {
+            Console.Write("enter the name: \n");
+            string name = Console.ReadLine();
+            Console.Write("enter the salary: \n");
+            if (int.TryParse(Console.ReadLine(), out int salary))
+            {
+                employee.Add((name, salary));
+                Console.WriteLine("Employee has been added");
+            }
+            else
+            {
+                Console.WriteLine("Invalid salary");
+            }
+        }
+
+        static void showList()
+        {
+            for(int i = 0; i < employee.Count; i++)
+            {
+                Console.WriteLine($"Name: {employee[i].name}, Salary: {employee[i].salary}");
+            }
         }
     }
 }
